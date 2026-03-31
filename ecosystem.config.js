475efe7f7e0c7d1155c699015aee9f0ebfd80895
SahanDhanujaya <sahandhanujaya18@gmail.com>
2026-03-31 18:49:03 +0530

    update ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,39 +1,43 @@
 module.exports = {
-  apps : [
+  apps: [
     {
       name: "cloud-sql-auth-proxy",
-      // Ensure the path to the binary is correct (e.g., ./cloud-sql-proxy)
       script: "./cloud-sql-proxy",
-      args: "tool-library-management:asia-southeast1:mysql-instance --private-ip",
+      args: "--private-ip --port=3306 tool-library-management:asia-southeast1:mysql-vm",
       instances: 1,
       exec_mode: "fork",
-      log_file: "./log/cloud-sql-proxy.log",
-      // Restarts the proxy if it crashes
-      autorestart: true
+      autorestart: true,
+      log_file: "./log/cloud-sql-proxy.log"
     },
+
     {
       name: "user-service",
-      script: "./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
-      interpreter: "java",
-      interpreter_args: "-jar",
-      log_file: "./log/user-service.log",
-      instances: 2
+      script: "java",
+      args: "-jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
+      instances: 2,
+      exec_mode: "cluster",
+      autorestart: true,
+      log_file: "./log/user-service.log"
     },
+
     {
       name: "tool-inventory-service",
-      script: "./tool-inventory-service/target/tool-inventory-service-0.0.1-SNAPSHOT.jar",
-      interpreter: "java",
-      interpreter_args: "-jar",
-      log_file: "./log/tool-inventory-service.log",
-      instances: 2
+      script: "java",
+      args: "-jar ./tool-inventory-service/target/tool-inventory-service-0.0.1-SNAPSHOT.jar",
+      instances: 2,
+      exec_mode: "cluster",
+      autorestart: true,
+      log_file: "./log/tool-inventory-service.log"
     },
+
     {
       name: "borrow-service",
-      script: "./borrow-service/target/borrow-service-0.0.1-SNAPSHOT.jar",
-      interpreter: "java",
-      interpreter_args: "-jar",
-      log_file: "./log/borrow-service.log",
-      instances: 2
-    },
+      script: "java",
+      args: "-jar ./borrow-service/target/borrow-service-0.0.1-SNAPSHOT.jar",
+      instances: 2,
+      exec_mode: "cluster",
+      autorestart: true,
+      log_file: "./log/borrow-service.log"
+    }
   ]
-}
+};
